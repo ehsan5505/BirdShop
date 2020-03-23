4eb9f2ca87def36c27e91216fe7a032aa77a6678
Ehsan Rafeeque <ehsan5505@gmail.com>
2020-03-23 13:32:39 +0500

    Refactor the code and make the Homepage in separate folder

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.styles.scss'
+
+import Directory from '../../components/directory-menu/directory.component';
+
+const HomePage = () => (
+    <div className="homepage">
+        <Directory />
+    </div> /* Homepage div ends here */
+)
+
+export default HomePage;
