3959783ae219ccb0e087d7b3a291a1afa33a6a43
Ehsan Rafeeque <ehsan5505@gmail.com>
2020-03-29 02:29:48 +0500

    Storing the Google Session in the Application by defining it with the user | also change the scope from function to component of App

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInSignOut from './pages/register/sign-in-sign-out.components';
+import { auth } from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -14,18 +15,42 @@ import './App.css';
 //   </div>
 // )
 
-function App() {
-  return (
-    <div> 
-      <Header/>
-      <Switch>
-        <Route exact='true' path='/' component={HomePage} />
-        <Route exact="true" path="/shop" component={ShopPage} />
-        <Route exact="true" path="/signin" component={SignInSignOut} />
-      </Switch>
+class App extends React.Component {
 
-    </div>
-  );
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser:null
+    }
+  }
+
+  unsubscribedAuthUser = null;
+
+  componentDidMount(){
+    unsubscribedAuthUser=auth.onAuthStateChanged(user => {
+      this.setState({currentUser:user})
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribedAuthUser();
+  }
+
+  render() {
+    return (
+      <div> 
+        <Header/>
+        <Switch>
+          <Route exact='true' path='/' component={HomePage} />
+          <Route exact="true" path="/shop" component={ShopPage} />
+          <Route exact="true" path="/signin" component={SignInSignOut} />
+        </Switch>
+  
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -6,7 +6,7 @@ import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
 
-import SignInWithGoogle, { signInWithGoogle } from '../../firebase/firebase.utils';
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
   constructor(props) {
