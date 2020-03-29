9495189aa3c7f58a498a44e05eeefeb6737bf852
Ehsan Rafeeque <ehsan5505@gmail.com>
2020-03-30 02:36:28 +0500

    add little tweaks to redirect to homepage when signIn and define Iterator for ActionTypes for ease to reference

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route,Switch} from "react-router-dom";
+import {Route,Switch,Redirect} from "react-router-dom";
 import { auth,createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 
@@ -52,7 +52,7 @@ class App extends React.Component {
         <Switch>
           <Route exact={true} path='/' component={HomePage} />
           <Route exact={true} path="/shop" component={ShopPage} />
-          <Route exact={true} path="/signin" component={SignInSignOut} />
+          <Route exact={true} path="/signin" render={ () => this.props.currentUser ? (<Redirect to='/' />) : (<SignInSignOut/>) } />
         </Switch>
   
       </div>
@@ -60,7 +60,11 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps = ({user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
