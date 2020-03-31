91ab45f57786abc2952503de032a34f07992e14d
Ehsan Rafeeque <ehsan5505@gmail.com>
2020-03-31 17:29:43 +0500

    add cart action to remove the item from the cart when press x on cartCheckout component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-
+import { clearItemFromCart } from '../../redux/cart/cart.actions';
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({cartItem:{imageUrl,name,price,quantity}}) => (
-    <div className='checkout-item'>
+const CheckoutItem = ({cartItem,clearItem}) => {
+    const {imageUrl,name,price,quantity} = cartItem;
+    return (<div className='checkout-item'>
         <div className='image-container'>
             <img src={imageUrl} alt='item' />
         </div>
         <span className='name'>{name}</span>
         <span className='quantity'>{quantity}</span>
         <span className='price'>{price}</span>
-        <div className='remove-button'>&#10005;</div>
+        <div className='remove-button' onClick={() => clearItem(cartItem)} >&#10005;</div>
     </div> 
-)
+)}
 
-export default CheckoutItem;
+const mapDispatchToProp = dispatch =>({
+    clearItem: item => dispatch(clearItemFromCart(item))
+})
+
+export default connect(null,mapDispatchToProp)(CheckoutItem);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -7,4 +7,9 @@ export const toggleCartHidden = () => ({
 export const addItem = item => ({
     type: cartActionTypes.ADD_ITEM,
     payload: item
-})
+});
+
+export const clearItemFromCart = item => ({
+    type: cartActionTypes.CLEAR_ITEM_FROM_CART,
+    payload: item
+});
