037829dfd2f55a52ba30cf3acaa5b166b2a99421
Ehsan Rafeeque <ehsan5505@gmail.com>
2020-04-01 17:07:22 +0500

    add a separate the redux directory to store the state-level information of directory for ease of reference and leave the directory class as a component

---
+++ src/redux/directory/directory.reducer.js
@@ -0,0 +1,38 @@
+const INTIAL_STATE = {
+    sections: [
+        {
+            id:1,
+            title: 'Medicines',
+            imageUrl: 'https://cdn11.bigcommerce.com/s-fa708/images/stencil/1280x1280/products/703/2648/Vetafarm_Probotic-90g1__83500.1502986111.jpg?c=2?imbypass=on'
+        },
+        {
+            id:2,
+            title: 'Feeds',
+            imageUrl: 'https://i.ytimg.com/vi/0Yqw22VCII4/maxresdefault.jpg'
+        },
+        {
+            id:3,
+            title: 'Cages',
+            imageUrl: 'https://apollo-singapore.akamaized.net/v1/files/fxifqda6pj14-PK/image;s=850x0'
+        },
+        {
+            id:4,
+            title: 'Birds',
+            imageUrl: 'https://cache.desktopnexus.com/thumbseg/2351/2351311-bigthumbnail.jpg'
+        },
+        {
+            id:5,
+            title: 'Pets',
+            imageUrl: 'https://www.fajarmag.com/wp-content/uploads/2018/12/pets_and_money_hero_image_-_minus_guinea.png'
+        }
+    ]
+}
+
+const directoryReducer = (state = INTIAL_STATE, action) => {
+    switch(action.type){
+        default:
+            return state
+    }
+};
+
+export default directoryReducer;
